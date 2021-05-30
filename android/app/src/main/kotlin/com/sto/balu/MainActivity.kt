b717506ae8460d281158f0c6804ebbdc0156f5eb
andriimakarenko <andrii.makarenko@euvic.pl>
2021-05-30 16:07:59 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/sto/balu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sto.balu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:balu_sto/main.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  runApp(MyApp(Colors.red));
+}
